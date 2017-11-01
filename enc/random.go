c5cf12a15da7e4f3aec6189bd7027f95967636a1
Jiajun Fan <celeheaven@gmail.com>
2017-11-01 17:04:25 +0800

    Create random.go

---
+++ enc/random.go
@@ -0,0 +1,13 @@
+package main
+
+import "math/rand"
+
+var kRandLetters = []byte("01234567890abcdefghijklmnopqrstuvwxzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringBytes(n int) []byte {
+    b := make([]byte, n)
+    for i := range b { 
+        b[i] = kRandLetters[rand.Intn(len(kRandLetters))]
+    }   
+    return b
+}
